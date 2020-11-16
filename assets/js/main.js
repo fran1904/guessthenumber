66ccd210964dc3582368da484222d4e72de15cfd
fran <jazzfran1@gmail.com>
2020-11-16 17:52:03 +0100

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,5 @@
+// guessNumber()has to:
+// roundCount++
+// replace the contents of div#roundsOutputElement with `${roundCount} / ${numOfRounds}`
+// store the user input from #user-guess in var userGuess
+//
