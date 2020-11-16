a9ab05e14d8de6984a5c011b86248c01837c561c
fran <jazzfran1@gmail.com>
2020-11-16 18:09:42 +0100

    planned guess function

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,11 @@
 // guessNumber()has to:
+
 // roundCount++
 // replace the contents of div#roundsOutputElement with `${roundCount} / ${numOfRounds}`
+
+
+// generate a random number between 1 and 100. Store in var theAnswer
 // store the user input from #user-guess in var userGuess
-//
+// if theAnswer == userGuess, output `Yes! You got me in ${roundCount} guesses! I'm ${userGuess}. <a href="#" onclick="reset()">You win! Play again?`</a>. 
+//if theAnswer > userGuess, output `${roundCount} - You need to guess higher than ${userGuess}`. 
+// else output `${roundCount} - You need to guess lower than ${userGuess}`.
