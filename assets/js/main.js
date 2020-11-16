d5795e1b8b35b212c2b360af1fb422576555b27a
fran <jazzfran1@gmail.com>
2020-11-16 18:32:53 +0100

    random finished

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,13 @@
 // guessNumber()has to:
-
-// roundCount++
+// // roundCount++
 // replace the contents of div#roundsOutputElement with `${roundCount} / ${numOfRounds}`
-
-
 // generate a random number between 1 and 100. Store in var theAnswer
+
+
+
+
+// Still ToDo:
+
 // store the user input from #user-guess in var userGuess
 // if theAnswer == userGuess, output `Yes! You got me in ${roundCount} guesses! I'm ${userGuess}. <a href="#" onclick="reset()">You win! Play again?`</a>. 
 //if theAnswer > userGuess, output `${roundCount} - You need to guess higher than ${userGuess}`. 
